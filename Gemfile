0fef3d7405f2cbc3b48cf8f2df5382b6e65aedc6
Kako Botasso <kakodev@gmail.com>
2013-01-10 21:29:09 -0200

    Criando o projeto

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source :rubygems
+
+gem "rails", "3.2.11"
+
+gem "mysql2"
+
+
+group :assets do
+  gem "sass-rails"
+  gem "therubyracer", :platforms => :ruby
+  gem "uglifier", ">= 1.0.3"
+end
+
+group :development, :test do
+	gem "rspec-rails"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackerboard::Application.config.secret_token = '8af8c747903832ef1d2592dd0afea62ad9701995c383f7160e3bea8379aa3fb32dc55e05ed9e7822cf5045a4c47bc51a8bc59126bcf854bcb89120328c32e897'
